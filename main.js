58168a7662bb5b64b179ff7fff9e2c4276db7e0a
laurendanger <lauren.koste@gmail.com>
2017-08-01 20:14:45 +1000

    commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// $(document).ready(showMore)
+//   {
+//   console.log('loaded')
+//
+//   $('#clickMexico').click(showMore)
+//
+//   function showMore(event) {
+//     debugger
+//     event.preventDefault();
+//     $('#mexicoPop').slideUp();
+//   }
+// });
+//
+// $(document).ready(showMore);
+// console.log('loaded')
+//   function showMore(event) {
+//     event.preventDefault();
+//     $('#mexicoPop').slideDown();
+//   }
+$(document).ready(console.log('Cool beans'))
